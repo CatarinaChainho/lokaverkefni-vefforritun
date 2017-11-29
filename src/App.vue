801a6531c94bbb5b9323331489a687503a95a6ce
CatarinaChainho <catarinachainhocosta@gmail.com>
2017-11-29 10:00:17 +0000

    style

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-     <div id="app" class="container">
-          <section class="panels">
-               <input type="radio" id="mark_all" v-bind:checked="areAllSelected" v-on:click="selectAll">
+     <div class="container" id="app">
+          <section class="panel">
+               <input type="checkbox" id="mark_all" v-bind:checked="areAllSelected" v-on:click="selectAll">
                <input type="text" placeholder="What Do You Need To Do?" autofocus class="text-input" v-model="newTask" v-on:keyup.enter="addTask">
                <button  v-on:click="addTask">Submit</button>
                <button v-on:click="clearList">Clear List</button>
           </section>
           <ul class="list">
                <li v-for="task in taskList" mv-bind:class="{done: task.checked}">
-                    <input type="radio" class="checkbox" v-model="task.checked">
+                    <input type="checkbox" class="checkbox" v-model="task.checked">
                     <input type="text" v-show="task.edit" v-model="task.edit">
                     <label for="checkbox" v-on:click="editTask(task)">{{ task.title }}</label>
-                    <button class="delete is-large" v-on:click="removeTask(task)">X</button>
+                    <button class="delete" v-on:click="removeTask(task)">X</button>
                </li>
           </ul>
      </div>
@@ -27,12 +27,11 @@
                }
           },
           computed: {
-               //if all the tasks are selected procceed to write them off
                areAllSelected: function(){
                     return this.taskList.every(function(task){
                          return task.checked;
                     }) && this.taskList.length > 0;
-               }, 
+               },
           },
 
           methods: {
@@ -45,22 +44,19 @@
                               checked: false,
                               edit: false,
                          });
-                    this.newTask = "";
+                         this.newTask = "";
                     }
                },
 
-               //removing the tasks from the list
                removeTask: function(task) {
                     var index = this.taskList.indexOf(task); 
                     this.taskList.splice(index, 1);
                },
 
-               //deleting the tasks from the list
                clearList: function() {
                     this.taskList = [];
                },
 
-               //selecting all the created tasks in the list
                selectAll: function(task){
                     var targetValue = this.areAllSelected ? false : true;
                     for (var i = 0; i < this.taskList.length; i++) {
@@ -75,7 +71,7 @@
                     }
                })
                .then(function(response) {
-                    console.log(response);
+                    console.log(response.data);
                }).
                catch(function(error) {
                     console.log(error);
@@ -85,104 +81,57 @@
 </script>
 
 <style lang="scss">
-     ul,
-     li {
-          margin: 0;
-          padding: 0;
-          border: 0;
-     }
-
-
-     /* Global Styles */
-     body {
-          line-height: 1;
-          font-family: "Lato", sans-serif;
-          background-color: #EFF1F2;
-     }
-
-     #app  {
+     .container {
           width: 70%;
-          margin: 1em auto 3em;
           border: 1px solid #efefef;
      }
 
-     .panels,
-     li {
-          /* Use flexbox */
-          display: flex;
-          /* Center everything inside .panels and li vertically */
-          align-items: center;
-          /* Distribute space evenly between the contents*/
-          justify-content: space-between;
-          list-style-type: none;
-          padding: 10px;
-          border-bottom: 1px solid #efefef;
-          background-color: #E7E8EB;
-     }
-
-     panels {}
-
-     .text-input {
-          border: 1px solid #dedede;
-          padding-left: 10px;
-          width: 70%;
-          height: 35px;
-          color: #555;
-     }
-
      button {
-          color: #555;
-          background-color: #FFFFFF;
-          border: 1px solid #bbb;
+          color : #915196;
+          background-color: #f8a3ff;
+          border: 1px solid #c4a3c6;
           outline: 0;
           width: 100px;
           height: 38px;
           cursor: pointer;
-          font-size: 12px;
+          font-size: 15px;
      }
 
-     .list li {
-          background-color: #CC0099;
+     .text-input {
+          border: 1px solid #7c99ff;
+          padding-left: 10px;
+          width: 70%;
+          height: 35px;
+          color: #0037ff;
      }
+
+     .list li{
+          background-color: #7c99ff;
+     }     
 
      .list li.done label {
-          color: #d32828;
-          text-decoration-line: line-through;
+          color: #d9d9d9;
+          text-decoration: line-through;
      }
-
      .list li .delete {
           background-color: transparent;
-          border: 1px solid #3465A4;
-          color: #ddd;
-          /* Hide the delete button by default*/
+          border: 1px solid #7c99ff;
+          color: #0037ff;
           visibility: hidden;
           font-size: 20px;
           font-weight: bold;
      }
-
      .list li:hover > .delete {
-          /* Show the delete button when hovering over each list item */
           visibility: visible;
      }
-
      .list label {
           padding-right: 10px;
           display: inline-block;
           width: 70%;
           font-size: 18px;
           line-height: 24px;
-          color: #fcfcfc;
+          color: #192551;
           z-index: 2;
           overflow: hidden;
-     }
-
-     @media screen and (max-width: 768px) {
-          .container {
-               width: 90%;
-               max-width: 90%;
-          }
-          button {
-               width: 80px;
-          }
      }
 </style>
